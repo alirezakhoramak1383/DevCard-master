6b4b09a81bcf00fa3b9b4f6fb0292364ec237ec4
alireza khorammak <alirezakhorammak@gmail.com>
2024-07-10 17:53:07 -0700

    Add Folder Data and Make Constractor

---
+++ Data/ProjectStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevCard.Models;
+
+namespace DevCard.Data
+{
+    public class ProjectStore
+    {
+        public static List<Project> GetProjects()
+        {
+            return new List<Project>
+            {
+                new Project(1,"تاکسی","project-1.jpg","در خواست تاکسی برای سفر های درون شهری","snapp"),
+                new Project(2,"زود فود ","project-2.jpg","در خواست آنلاین غذا برای سراسر کشور","zoodfood"),
+                new Project(3,"مدارس","project-3.jpg","سیستم مدیریت یکپارچه مدارس","MONOP"),
+                new Project(4,"فضا پیما","project-4.jpg","برنامه مدیریت فضا پیما های ناسا","NASA")
+            };
+        }
+    }
+}
--- ViewComponents/ProjectViewComponent.cs
+++ ViewComponents/ProjectViewComponent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevCard.Data;
 using DevCard.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,13 +12,7 @@ namespace DevCard.ViewComponents
     {
         public IViewComponentResult Invoke()
         {
-            var projects=new List<Project>
-            {
-                new Project(1,"تاکسی","project-1.jpg","در خواست تاکسی برای سفر های درون شهری","snapp"),
-                new Project(2,"زود فود ","project-2.jpg","در خواست آنلاین غذا برای سراسر کشور","zoodfood"),
-                new Project(3,"مدارس","project-3.jpg","سیستم مدیریت یکپارچه مدارس","MONOP"),
-                new Project(4,"فضا پیما","project-4.jpg","برنامه مدیریت فضا پیما های ناسا","NASA")
-            };
+            var projects = ProjectStore.GetProjects();
             return View("_project", projects);
         }
     }
